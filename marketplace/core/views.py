03727d57026f2c84ee787e1a550162a8bd37b8c0
Angie Duhard <angie.duhard@gmail.com>
2023-08-18 17:12:55 +0200

    added category model

--- marketplace/core/views.py
+++ marketplace/core/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'core/index.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+def privacy(request):
+    return render(request, 'core/privacy.html')
+
+def terms(request):
+    return render(request, 'core/terms.html')
--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -2,9 +2,17 @@ from django.contrib import admin
 from django.urls import path
 
 from core.views import index
+from core.views import contact
+from core.views import privacy
+from core.views import terms
 
 urlpatterns = [
     # set homepage to index view
     path('', index, name='index'),
+    # set contact page, privacy page, and terms page
+    path('contact/', contact, name='contact'),
+    path('privacy/', privacy, name='privacy'),
+    path('terms/', terms, name='terms'),
+
     path('admin/', admin.site.urls),
 ]
