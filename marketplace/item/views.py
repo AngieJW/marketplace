5a54723bbdf969293d942319d0fbed0a7c1d3c76
Angie Duhard <angie.duhard@gmail.com>
2023-08-19 15:40:23 +0200

    added conversation and message models

--- marketplace/item/views.py
+++ marketplace/item/views.py
@@ -7,8 +7,12 @@ from item.models import Item, Category
 
 def items(request):
     query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
     items = Item.objects.filter(is_sold=False)
     categories = Category.objects.all()
+
+    if category_id:
+        items = items.filter(category=category_id)
 
     if query:
         items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
@@ -17,6 +21,7 @@ def items(request):
         'items': items,
         'query': query,
         'categories': categories,
+        'category_id': int(category_id),
     })
 
 # Create your views here.
